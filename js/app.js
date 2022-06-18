f0153da1a8acc9f44d1bf42c0482491eee282d61
Yahya <yahyanaeem2017@gmail.com>
2022-06-18 16:28:24 +0500

    add files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("#header-wrapper").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    responsive: {
+      0: {},
+      600: {},
+      1000: {},
+    },
+  });
+});
