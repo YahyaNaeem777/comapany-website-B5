ee1e9328f20f882c71d19c3305ee3d9698d6b888
Yahya <yahyanaeem2017@gmail.com>
2022-06-29 17:55:06 +0500

    updated files and add work section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 $(document).ready(function () {
-  $("#header-wrapper").owlCarousel({
+  $(".owl-carousel").owlCarousel({
     loop: true,
-    margin: 0,
+    margin: 23,
     nav: true,
-    items: 1,
+    items: 3,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    center: true,
     responsive: {
-      0: {},
-      600: {},
-      1000: {},
+      0: {
+        items: 1,
+      },
+      700: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
     },
   });
 });
